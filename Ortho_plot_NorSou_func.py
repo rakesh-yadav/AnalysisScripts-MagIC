89906e4406032e8bfaedd716425e43e6a4817aa2
Rakesh Yadav <raku87@gmail.com>
2019-09-01 09:44:31 -0400

    Helper python function for orthographic plotting
    Plots North and south view of lat-lon data on an othographic projection using python Basemap. It cal also plot contours of another data set. If SHTNS is installed properly, then it can low-pass the contour data for smoother contour plot. For loading in a script, use "import sys; sys.path.append('path to function file'); from Ortho_plot_NorSou_func import Ortho_plot"

---
+++ Ortho_plot_NorSou_func.py
@@ -0,0 +1,70 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+
+def Ortho_plot(data, ind=0, skip=0, cmap='RdBu_r', vmin=-30000, save_path='/home/rakesh/Desktop', dpi=150, label=r'$(\nabla\times{\bf V})_z$', contour=False, data_cont=[], vmin_cont=1000, l_max_trunc_cont=40):
+
+ data = np.clip(data, vmin, -vmin)
+
+ lons = np.linspace(-180, 180, data.shape[0])
+ lats = np.linspace(-90, 90, data.shape[1])
+
+ fig = plt.figure(figsize=(11,5))
+
+ # North view
+ ax = fig.add_axes([0.005, 0.01, 0.45, 0.97])
+ map = Basemap(projection='ortho',lat_0=70,lon_0=90,resolution=None)
+ map.drawmeridians(np.arange(0,360,45), dashes=[200,1], latmax=90)
+ map.drawparallels([-60, -30, 30, 60], dashes=[200,1], latmax=90)
+ map.drawparallels([0], dashes=[200,1])
+ new_dat = map.transform_scalar(data.T, lons, lats, \
+                                data.shape[0], \
+                                data.shape[1], masked=True)
+ im = map.imshow(new_dat, cmap=plt.get_cmap(cmap))
+ map.drawmapboundary()
+
+ if contour==True:
+  # add location of shtns library for tra=uncating data
+  import sys
+  sys.path.append('/media/rakesh/Seagate Expansion Drive1/Data2/Polar_vortex_runs/Movies_and_plots/Paper_plots')
+  try:
+   from ltruncate_shtns_function import shtns_truncate
+  except:
+   import sys
+   sys.exit('******SHTNS load unsuccessful. Try conda deactivate.*******')
+
+  # truncate data
+  data2 = shtns_truncate(data_cont[:-1,:], l_max_trunc=l_max_trunc_cont).T
+
+  llons, llats = np.mgrid[-np.pi:np.pi:data2.shape[0]*1j,
+                -np.pi/2.:np.pi/2.:data2.shape[1]*1j]
+  x, y = map(llons*180./np.pi, llats*180./np.pi)
+  levels = np.arange(vmin_cont, data2.max(), 300)
+  map.contour(x,y,data2,levels,colors='k',linewidths=[1.5], alpha=0.4)
+  levels = np.arange(data2.min(), -vmin_cont, 300)
+  map.contour(x,y,data2,levels,colors='k',linewidths=[1.5], linestyles='dashed', alpha=0.4)
+  print('Contours plotted')
+
+
+
+ # South view
+ ax = fig.add_axes([0.45, 0.01, 0.45, 0.97])
+ map = Basemap(projection='ortho',lat_0=-70,lon_0=90,resolution=None)
+ map.drawmeridians(np.arange(0,360,45), dashes=[200,1], latmax=90)
+ map.drawparallels([-60, -30, 30, 60], dashes=[200,1], latmax=90)
+ map.drawparallels([0], dashes=[200,1])
+ new_dat = map.transform_scalar(data.T, lons, lats, \
+                                data.shape[0], \
+                                data.shape[1], masked=True)
+ im = map.imshow(new_dat, cmap=plt.get_cmap(cmap))
+ map.drawmapboundary()
+ cax = fig.add_axes([0.90, 0.04, 0.02, 0.8])
+ cbar = fig.colorbar(im,cax=cax,extend='both')
+
+ fig.text(0.88, 0.9, label, fontsize=20)
+ filename = save_path+'/img%05d.png' % (ind+skip)
+ fig.savefig(filename, dpi=dpi)
+ im.lines = []
+ fig.clf()
+
+ print('Frame saved at', filename)
